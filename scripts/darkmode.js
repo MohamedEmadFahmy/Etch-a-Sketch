43a58062a33ab2aa1e905980f54f3a5e45ffecfd
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2023-05-09 01:57:04 +0200

    Add dark mode

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,11 +1,16 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme');
+const picker = document.querySelector("#color-picker");
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
   
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
+        picker.setAttribute("value", "#F01818");
+    }
+    else{
+
     }
 }
 
@@ -13,9 +18,12 @@ function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
+        picker.setAttribute("value", "#F01818");
     }
-    else {        document.documentElement.setAttribute('data-theme', 'light');
-          localStorage.setItem('theme', 'light');
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        picker.setAttribute("value", "#000000");
     }    
 }
 
