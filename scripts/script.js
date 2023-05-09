889a17528bf5641a4f4979ffc8a860be52d22fcb
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2023-05-09 02:59:02 +0200

    Finish styling of website You can only click one of the first 3 buttons now Implement showGridlines click option implement click option for color mode selection

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,55 @@
-let gridSizeText = document.querySelector("div#tools p");
-let gridSizeSlider = document.querySelector("#grid-size");
-
-console.log(gridSizeSlider);
-
+const gridSizeText = document.querySelector("div#tools p");
+const gridSizeSlider = document.querySelector("#grid-size");
 
 let gridSize = gridSizeSlider.value;
 
-gridSizeText.innerHTML = gridSize + "x" + gridSize;
+const buttons  = document.querySelectorAll("button.mode");
+
+const firstButton = document.querySelector("button#color-mode"); 
+
+const clearButton = document.querySelector("button#clear"); 
+
+const gridlinesButton = document.querySelector("button#show-gridlines"); 
+
+
+firstButton.classList.add("active"); // set default mode and button to be active
+
+let mode = "color";
+
+
+const handleClick = (event)=>{
+    buttons.forEach((button) => {
+        button.classList.remove("active");
+    });
+    event.target.classList.add("active");     // to make only 1 of the first 3 buttons active at the same time
+    let buttonId = event.target.id;
+    mode = buttonId.slice(0,-5)
+    console.log(mode);
+};
+
+buttons.forEach((button) =>{
+    button.addEventListener("click", handleClick);
+});
+
+
+gridSizeText.innerHTML = gridSize + "x" + gridSize;  // to update slider text
 
 gridSizeSlider.oninput = () => {
     gridSize = gridSizeSlider.value;
     gridSizeText.innerHTML = gridSize + "x" + gridSize;
 }
+
+
+const clear = ()=>{
+    
+};
+
+
+const showGridlines = () => {
+    gridlinesButton.classList.toggle("active");
+};
+
+
+clearButton.addEventListener("click", clear);
+gridlinesButton.addEventListener("click", showGridlines);
+
