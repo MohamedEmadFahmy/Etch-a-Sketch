c4071b53a2919fd4771968b7cef4db9c8f9428ea
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2023-05-11 03:46:12 +0200

    Make grid slider fully work Add showGridlines buton functionality showGridlines functionality doesnt work 100%

--- scripts/script.js
+++ scripts/script.js
@@ -11,11 +11,46 @@ const clearButton = document.querySelector("button#clear");
 
 const gridlinesButton = document.querySelector("button#show-gridlines"); 
 
+const grid = document.querySelector("#grid");
+
+const pixelsXY = 700;
+
 
 firstButton.classList.add("active"); // set default mode and button to be active
 
 let mode = "color";
+let showGridLines = true;
+gridlinesButton.classList.toggle("active");
 
+
+
+const clear = ()=>{
+    
+};
+
+const toggleGridlines = () => {
+    gridlinesButton.classList.toggle("active");
+    let borderLeftCells = document.querySelectorAll(".border-left");
+    let borderTopCells = document.querySelectorAll(".border-top");
+    if(showGridLines){
+        borderLeftCells.forEach(cell => {
+            cell.style.borderLeft = "1px solid grey";
+        });
+        borderTopCells.forEach(cell => {
+            cell.style.borderTop = "1px solid grey";
+        });
+    }
+    else{
+        borderLeftCells.forEach(cell => {
+            cell.style.borderLeft = "0px solid grey";
+        });
+        borderTopCells.forEach(cell => {
+            cell.style.borderTop = "0px solid grey";
+        });
+    }
+};
+
+toggleGridlines();
 
 const handleClick = (event)=>{
     buttons.forEach((button) => {
@@ -32,24 +67,53 @@ buttons.forEach((button) =>{
 });
 
 
+const createDivs = (size) => {
+    for(let i = 0; i < size; i++){
+        for(let j = 0; j < size; j++){
+            const div = document.createElement("div");
+            div.classList.add("box");
+            div.style.add
+            let width = (pixelsXY * 0.92988) / size;
+            div.style.width = width + "px";
+            div.style.height = width + "px";
+            if(i !== 0){
+                div.classList.add("border-left");
+                // div.style.borderLeft = "1px solid grey";
+                // div.style.backgroundColor = "black";
+            }
+            if(j !== 0){
+                div.classList.add("border-top");
+                // div.style.borderTop = "1px solid grey";
+                // div.style.backgroundColor = "black";
+            }
+            grid.appendChild(div);
+        }
+    }
+};
+
+
 gridSizeText.innerHTML = gridSize + "x" + gridSize;  // to update slider text
+// createDivs(gridSize);
+createDivs(4);
+
+
 
 gridSizeSlider.oninput = () => {
+    grid.innerHTML = "";
     gridSize = gridSizeSlider.value;
     gridSizeText.innerHTML = gridSize + "x" + gridSize;
+    // createDivs(2);
+    createDivs(gridSize);
+    if(showGridLines){
+        toggleGridlines();
+    }
 }
 
 
-const clear = ()=>{
-    
-};
-
-
-const showGridlines = () => {
-    gridlinesButton.classList.toggle("active");
-};
-
 
 clearButton.addEventListener("click", clear);
-gridlinesButton.addEventListener("click", showGridlines);
+gridlinesButton.addEventListener("click", () => {
+    toggleGridlines();
+    showGridLines = !showGridLines;
+});
 
